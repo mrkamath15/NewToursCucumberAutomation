98a0cc28f365f08293dd61022f05b6aaddce97ee
rajendrakamath <mrkamath@gmail.com>
2023-04-16 11:48:57 +0530

    extent reports integration

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         features = {".//src//test//resources//features"},
         glue = {"stepDefinitions"},
         dryRun = false,
-        monochrome = true
+        monochrome = true,
+        plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json", "junit:target/cucumber.xml","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
--- src/test/java/stepDefinitions/MasterHooks.java
+++ src/test/java/stepDefinitions/MasterHooks.java
@@ -2,6 +2,9 @@ package stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import utils.DriverFactory;
 
 public class MasterHooks extends DriverFactory {
@@ -12,7 +15,8 @@ public class MasterHooks extends DriverFactory {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        scenario.attach(((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES), "image/png", scenario.getName());
         driver.quit();
     }
 }
